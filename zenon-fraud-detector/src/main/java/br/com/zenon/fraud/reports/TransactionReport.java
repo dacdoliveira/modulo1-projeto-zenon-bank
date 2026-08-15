a77312540ecae5365c1321e6b0f420803a318687
dacdoliveira <55628845+dacdoliveira@users.noreply.github.com>
2026-08-15 19:16:18 -0300

    tarefa/08-i18n (#8)
    Cria métodos para internacionalização de mensagem e moeda
    lê argumento do usuário informando language, a partir dele obtem locale e define idioma e moeda

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/reports/TransactionReport.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/reports/TransactionReport.java
@@ -7,31 +7,70 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Optional;
+import java.util.ResourceBundle;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Stream;
 
 public class TransactionReport {
-    public static void readFile(String filePath) throws IOException {
+    public static void readFile(String filePath,  String language) throws IOException {
+
         Path path = Path.of(filePath);
         Stream<String> lines = Files.lines(path);
         long totalLines  = lines.count();
-        System.out.println("Total de linhas: " + totalLines);
+        System.out.println(getTranslateText("zeno.fraud.detector.msg.total_linhas", language) +": " + totalLines);
         lines = Files.lines(path);
         long totalFrauds = lines.filter(TransactionReport::isFraud).count();
-        System.out.println("Total de fraudes: " + totalFrauds);
+        System.out.println(getTranslateText("zeno.fraud.detector.msg.total_fraudes", language) +": " + totalFrauds);
         lines = Files.lines(path);
         BigDecimal totalAmount = lines.map(TransactionReport::amount)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
-        System.out.println("Valor total transacionado: " + totalAmount);
+        System.out.println(getTranslateText("zeno.fraud.detector.msg.valor_total", language) +": " + getFormattedCurrency(totalAmount, language));
 
     }
 
-    static void main() throws IOException {
-        readFile("./data/arquivo.csv");
+    static void main(String[] args) throws IOException {
+        String language = (args!=null && args.length>0)? args[0] : "pt";
+
+        readFile("./data/arquivo.csv", language);
+
+
+
     }
 
+    private static String getFormattedCurrency(BigDecimal value, String language){
+        if (value==null ) return null;
+        Locale locale = null;
+        if ("en".equalsIgnoreCase(language)){
+            locale = Locale.of("en","US");
+        } else if ("pt".equalsIgnoreCase(language)){
+            locale =  Locale.of("pt","BR");
+        } else{
+            System.out.println("language não identificada, será usado o locale default");
+            locale = Locale.getDefault();
+        }
+        NumberFormat nf = NumberFormat.getCurrencyInstance(locale);
+        return nf.format(value);
+
+    }
+    private static String getTranslateText(String prop, String language){
+        if (prop==null || prop.isEmpty()) return null;
+
+        Locale locale = null;
+        if ("en".equalsIgnoreCase(language)){
+            locale = Locale.of("en","US");
+        } else if ("pt".equalsIgnoreCase(language)){
+            locale =  Locale.of("pt","BR");
+        } else{
+            System.out.println("language não identificada, será usado o locale default");
+            locale = Locale.getDefault();
+        }
+        ResourceBundle rsBundle = ResourceBundle.getBundle("report", locale);
+        return rsBundle.getString(prop);
+    }
     public static boolean isFraud (String line){
         if( line.startsWith("step")) return false;// Verifica se é cabeçalho
 
